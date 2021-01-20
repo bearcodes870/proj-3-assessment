3c3c76814e82fd99e139e86eb2303558e16d9a70
Bear <bear.ranasinghe@gmail.com>
2021-01-19 20:53:43 -0500

    model works

--- main_app/models.py
+++ main_app/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class Widget(models.Model):
     description = models.CharField(max_length=200)
-    quantity = models.IntegerField
+    quantity = models.IntegerField()
