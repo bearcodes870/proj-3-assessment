e6a20c76add791926e4d0ec67ee49a254176d678
Bear <bear.ranasinghe@gmail.com>
2021-01-19 20:36:33 -0500

    widget model implemented

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Widget
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    widgets = Widget.objects.all()
+    return render(request, 'home.html', { 'widgets': widgets })
